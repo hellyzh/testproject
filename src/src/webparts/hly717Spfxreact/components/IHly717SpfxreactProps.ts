a75f279a6d97b23423b7e28c78677130f72374ef
Helly Zhang <49181894+hellyzh@users.noreply.github.com>
2024-07-17 01:41:27 +0000

    init

---
+++ src/src/webparts/hly717Spfxreact/components/IHly717SpfxreactProps.ts
@@ -0,0 +1,7 @@
+export interface IHly717SpfxreactProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/src/webparts/hly717Spfxreact/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IHly717SpfxreactWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'Hly717SpfxreactWebPartStrings' {
+  const strings: IHly717SpfxreactWebPartStrings;
+  export = strings;
+}
